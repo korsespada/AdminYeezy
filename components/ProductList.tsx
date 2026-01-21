62a58307541cb9fe16a9d7a5c33a2cea954325a3
korsespada <korsespada@gmail.com>
2026-01-21 21:34:59 +0300

    Change grid layout to 2 columns on mobile

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -541,7 +541,7 @@ export default function ProductList({ initialData, brands, categories }: Product
 
       {/* Grid View */}
       {viewMode === 'grid' && (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
           {products.map((product) => (
             <div
               key={product.id}
